d8e53a4005392bc688144d8c0b28a2527ba78867
alain.tchougbo <alain.tchougbo@epitech.eu>
2024-11-30 18:12:39 +0100

    fix bug in useChannel

--- src/Hooks/useChannel.ts
+++ src/Hooks/useChannel.ts
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import axios from "axios"
+//  import axios from "axios"
 import { ChannelInfoType, ChannelPlaylistsType, HomeVideoCardType } from "../utils/Types"
 import { getActivities, getChannelInfo } from "../utils/api"
 import { getActvitiesVideos, getChannelPlaylists } from "../utils/api"
 import { fetchVideosWithChannels } from "../utils/videoDetailsHelper"
 import { parseChannelPlaylists } from "../utils/parseData"
 
-const API_KEY = import.meta.env.VITE_API_KEY
+// const API_KEY = import.meta.env.VITE_API_KEY
 
 interface ChannelVideoListState {
     videos: HomeVideoCardType[],
@@ -42,7 +42,7 @@ export const useChannel = () => {
 
     }
 
-    const fetchChanneldata = async (channelId: string, pageToken?: string) => {
+    const fetchChanneldata = async (channelId: string) => {
         if (category == "videos") {
 
             const channelVideosResponse = await getActivities(channelId, channelVideoList!.nextPageToken!)
